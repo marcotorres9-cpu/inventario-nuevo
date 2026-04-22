5b3e7dcf79145755eef8f4cb2bd4fb6ee6ef9395
Z User <z@container>
2026-04-22 04:41:38 +0000

    fix: remove outputFileTracingRoot para Vercel build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Force this directory as root to avoid workspace detection issues
-  outputFileTracingRoot: '/home/z/my-project/inventario-nuevo',
   async redirects() {
     return [
       {
